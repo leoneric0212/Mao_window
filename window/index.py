4b47f2ed220202feb4bfeda26a51f0722d6a3180
leoneric0212 <leoneric0212@gmail.com>
2024-05-28 16:49:03 +0800

    modify

---
+++ window/index.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+from tkinter import ttk
+
+def get_names() -> list[str]:
+    with open('names.txt',encoding='utf-8') as file:    
+        content:str=file.read()      
+    names:list[str]=content.split()
+    return names
+
+class Window(tk.Tk):
+    def __init__(self,title:str='Hello',**kwargs):
+        super().__init__(**kwargs)
+        self.title(title)
+        label:ttk.Label=ttk.Label(self,
+                                  text='Hello World',
+                                  font=('Arial',40,'bold'),
+                                  foreground='#ae1')
+        label.pack(padx=100,pady=40)
+
+if __name__=='__main__':
+    names:list[str]=get_names()
+    window:Window= Window(title='拍森')
+    window.mainloop()
---
+++ window_layout/README.md
@@ -0,0 +1,6 @@
+## index1.py
+![index1.py](./images/pic1.png)
+
+...
+## index2.py
+![index2.py](./images/pic2.png)
